ce7ebc49feb5ec56fb946cbb5ae9b23cb98cd94b
ashirshov <ashirshov@quantumbrains.com>
2023-12-14 21:04:27 +0300

    add llm pipelines

--- celery/tasks.py
+++ celery/tasks.py
@@ -2,25 +2,29 @@ import os
 import time
 
 from celery import Celery
+from model_utils import *
+from prompts import *
 
 RMQ_URL = os.getenv("RMQ_URL")
 REDIS_URL = os.getenv("REDIS_URL")
 
 celery_app = Celery("tasks", broker=RMQ_URL, backend=REDIS_URL)
 
+#define llm pipeline component
+llm = load_intel_model()
+agent_prompts = get_candidates_prompt()
+vacancy_prompt = get_vacancies_prompt()
+recs_prompt = get_recs_prompt()
+score_prompt = get_score_prompt()
 
 @celery_app.task(name="llm.recs")
 def process_recs(recs_form: dict) -> dict:
     vacancy = recs_form["vacancy_text"]
     cv = recs_form["cv_text"]
 
-    example_score = 87.65
-    example_text = f"""
-Текст вакансии:
-{vacancy}
-Текст резюме:
-{cv}
-        """
-    time.sleep(5)
+    candidate_info = get_candidate_info(agent_prompts, cv, llm)
+    key_vacancy= get_vacancy_info(vacancy_prompt, vacancy, llm)
+    recs_pred = get_recs(recs_prompt, candidate_info, key_vacancy, llm)
+    score = get_score(score_prompt, candidate_info, key_vacancy, llm)
 
-    return {"score": example_score, "recs": example_text}
+    return {"score": score, "recs": recs_pred}
